2dd36f222fdfc53827cff77f47cd71e6f4351777
Kharatishvilix <Kharatishvililuka@gmail.com>
2023-07-13 22:25:54 +0400

    initial commit

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Luka  Kharatishvili on 13.07.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            VStack{
+                    Spacer()
+                    Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                Spacer()
+                Image("button")
+                    .padding()
+                Spacer()
+
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                            
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+            }
+            
+        }
+
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
